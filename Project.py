6fbe32f040f0ccb587e2c361883b9f17acafa3cd
aniket-bhai <abcdkumarsai@gmail.com>
2025-03-11 10:34:28 +0530

    add function for face detection

--- Project.py
+++ Project.py
@@ -5,6 +5,14 @@ import cv2 as cv
 #including default camera
 cap=cv.VideoCapture(0)
 
+#including file
+face_cascade=cv.CascadeClassifier(r"C:\Users\ANIKET KUMAR\OneDrive\Desktop\AI Project\haarcascade_frontalface_default.xml")
+
+#checking file is loaded or not
+if face_cascade.empty():
+    print("file is not loaded...")
+    exit()
+
 #capture the device widthxheight
 frame_width=int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 frame_height=int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
@@ -19,7 +27,14 @@ while cap.isOpened():
     ret,frame=cap.read()
     if ret is True:
         frame=cv.resize(frame,(frame_width,frame_height))
+        gray_img=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+        faces=face_cascade.detectMultiScale(gray_img,1.1,8)
+        
+        for (x,y,w,h) in faces:
+            cv.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),1)
+            
         cv.imshow('frames',frame)
+        
         if cv.waitKey(1) & 0xff==ord('q'):
             break
     else:
